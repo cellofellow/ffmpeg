b87d2ad1a5f7d2153400fdd7a8c36d4dbcd2abae
ACUVE <sub_chon@yahoo.co.jp>
2014-11-25 10:59:35 +0900

    Use 8 processes when making

--- script/build.sh
+++ script/build.sh
@@ -17,21 +17,21 @@ git clone --depth 1 https://github.com/mulx/aacgain.git
 
 cd /usr/local/src/l-smash
 ./configure
-make -j 4
+make -j 8
 make install
 
 # Build libx264
 
 cd /usr/local/src/x264
 ./configure --enable-static
-make -j 4
+make -j 8
 make install
 
 # Build libx265
 
 cd /usr/local/src/x265/build/linux
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr ../../source
-make -j 4
+make -j 8
 make install
 
 # Build libfdk-aac
@@ -39,14 +39,14 @@ make install
 cd /usr/local/src/fdk-aac
 autoreconf -fiv
 ./configure --disable-shared
-make -j 4
+make -j 8
 make install
 
 # Build libvpx
 
 cd /usr/local/src/libvpx
 ./configure --disable-examples
-make -j 4
+make -j 8
 make install
 
 # Build libopus
@@ -54,24 +54,24 @@ make install
 cd /usr/local/src/opus
 ./autogen.sh
 ./configure --disable-shared
-make -j 4
+make -j 8
 make install
 
 # Build ffmpeg.
 
 cd /usr/local/src/ffmpeg
 ./configure --extra-libs="-ldl" --enable-gpl --enable-libass --enable-libfdk-aac --enable-libmp3lame --enable-libopus --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265 --enable-nonfree
-make -j 4
+make -j 8
 make install
 
 # Build aacgain
 
 cd /usr/local/src/aacgain/mp4v2
-./configure && make -k -j 4 # some commands fail but build succeeds
+./configure && make -k -j 8 # some commands fail but build succeeds
 cd /usr/local/src/aacgain/faad2
-./configure && make -k -j 4 # some commands fail but build succeeds
+./configure && make -k -j 8 # some commands fail but build succeeds
 cd /usr/local/src/aacgain
-./configure && make -j 4 && make install
+./configure && make -j 8 && make install
 
 # Remove all tmpfile 
 
